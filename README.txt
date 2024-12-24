9eb4b1a7fdd8df6c8655290391d205092d5e80fe
scot0308 <scot0308@gmail.com>
2024-12-24 12:28:23 -0600

    Update main.py to display our findings in the terminal. Also, add requirements.txt to allow for better portability.

---
+++ README.txt
@@ -0,0 +1,50 @@
+This project was used to enhance an analysis done on a data set that included
+MA crime records during year of 2010. To be more specific, it tracks different kind
+of crimes.  All different types of crimes are then grouped into two different bins.
+This project uses data from two different years and also includes unemployment rates during those year.
+
+The primary goal is to use a series of libraries to capture and export the data into a mysql
+database.  Then use data persistence to allow us to use python libraries to analyze the data.  This analysis
+primarily uses Hugging Face Transformers to analyze 500 random imported crime related tweets from the city
+of Boston that were made during 2022.  The Transformers library uses Piplines to analyze a text field to determine
+the probability of whether the tweet is a violent and non-violent crime.  When running the main code (main.py) data will be
+produced in the terminal that shows the probability that the library function is that it correctly analyzed the tweet as one of the two different categories.
+Based on these results each tweet is grouped into the correct categorical bin. This will be displayed in the console.
+
+
+
+Below is a list of steps to run this system.
+1.  Download and unzip this package and note the package location
+2.  Download mysql and mysql work bench and run a local instance of mysql to prepare for imports.
+3.  Go to MA_Crime_DB_Setup/create_db.py and modify the database fields to reflect your local environment in order to create a connection to your sql server.
+4.  Also, update the file path within create_db.py to reflect where this package is located and where the .csv files to be uploaded are located.
+5.  Run "pip install -r requirements.txt" within your pycharm or other IDE terminal to install all the necessary libraries needed to run the code.
+6.  Within sqlserver workbench right click on the server and select "refresh all", then Verify that the database and two tables where created and are populated with data.
+7.  Go to main.py and update database credentials to reflect your local environment.
+8.  Analyze results in the terminal.
+9.  Feel free to add to the project ideas include:
+
+A. Populating more data from different years and use transformers to create a predictive model for future years based on population
+and unemployment rates.
+
+B. Compare the city of Boston results to another city of a different size and determine how the size of a city population
+effects crime.
+
+C. Use my x_data_collection.py method to grab tweets in real time before using your monthly api call allowance.
+
+Some issues I encountered were the following:
+
+1.  Originally I intended to use 'X' tweepy 2.0 api to import a series of tweets in real time and then user the Hugging Face Transformers tool to interpret the conversational AI.
+Unfortunately, the free tier developer account is restricted to a total of no more than 1,500 total tweets.  Also, it restricts users from accessing the api more than 300 times every 15 minuest.
+
+I left the class in the code x_data_collection.py in the package.  You'll see that I created a method to handle the rate limit.
+Unfortunately, by the time my code was working well I had already extracted the total number of tweets allowed per month using the free tier
+
+To address this issue I created a class that uses the csv and random library to create random violent and non-violent crime tweets:
+
+tweet_csv_creation.py
+
+This class creates an adjustable amount of fake tweets using a list of violent and non-violent key words.
+Also, the amount of violent or non-violent tweets is also adjustable.
+It then uses the csv library to create the tweets csv that is used to generate the crime_tweets table in our ma_crime database.
+
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # main.py
+from MA_Crime_DB_Setup.tweet_csv_creation import non_violent_crimes
 from db_connector import DatabaseConnector
 from tweet_analysis_transformers import classify_tweets, compute_percentages
-from sqlalchemy import create_engine
 import pandas as pd
+
+# step 1: install mysql and determine the values of the following credentials.
+# NOTE: Credentials will be different for each user and where you store the package from this system.
 
 # Database connection details
 DB_USERNAME = 'root'
@@ -12,35 +15,23 @@ DB_PORT = '3306'
 DB_NAME = 'ma_crime'
 
 db_connector = DatabaseConnector(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+### Step 2: run db_main.py file within the MA_Crime_DB_Setup folder prior to running this.  The db_main.py
+### uses .csv files located within this package to setup the data needed to run the main.py file.
+### NOTE: only run the db_main.py file one time.
 
-# Step 1: Fetch data from the database and analyze the data
+### Step 2: Fetch data from the database and analyze the data
 query = '''
- SELECT cu.Non_Violent_Crimes_per_10_000_2010, cu.Violent_Crimes_per_10_000_2010, cu.Total_Crimes_per_10_000_2010,
- cu.Unemployment_Aug_2010, cu.Non_Violent_Crimes_per_10_000_2022, cu.Violent_Crimes_per_10_000_2022, cu.Total_Crimes_per_10_000_2022, 
- ct.id, ct.crime_tweets
- FROM ma_crime_unemployment cu
- INNER JOIN crime_tweets ct ON ct.city = cu.city
+ SELECT id, crime_tweets
+ FROM crime_tweets 
+ WHERE city = 'Boston'
  '''
-print(f'the query is {query} fetching now')
+print(f'Fetching tweets from crime_tweets table in the database.', end="\n\n")
 
-data = db_connector.fetch_data(query) # Display the first few rows
-columns = [
-    "Non_Violent_Crimes_per_10_000_2010", "Violent_Crimes_per_10_000_2010", "Total_Crimes_per_10_000_2010",
-    "Unemployment_Aug_2010", "Non_Violent_Crimes_per_10_000_2022", "Violent_Crimes_per_10_000_2022",
-    "Total_Crimes_per_10_000_2022"
-]
+data = db_connector.fetch_data(query)
 
-df = pd.DataFrame(data, columns=columns)
-print("Data fetched successfully:")
-print(df)
-
-# Initialize dictionary for access to the query values outside of the for loop
-crime_data_dict = []
-
-# Store all the data in a dictionary
-for index, row in df.iterrows():
-    crime_data_dict.append({column: row[column] for column in df.columns})
-
+### Step 3: analyze the data using the results from the database.  Use Transformers to determine
+### the probability that the crime commited within the tweet is violent or non-violent.  Then assign each
+### tweet to the appropriate categorical bin.
 
 # Rename the tweet columns to match what classify_tweets transformer method expects
 data = data.rename(columns={
@@ -51,41 +42,139 @@ data = data.rename(columns={
 # extract only needed columns
 data_as_dicts = data[['tweet_id', 'tweet_text']].to_dict('records')
 results = classify_tweets(data_as_dicts)
-print(f'the results of the tweet_analysis using pipeline from  transformers is {results}')
 
+# display each tweet as a list of dictionaries that contains the probability of whether the tweet falls into one of the
+# two categories based on the Hugging Face transformers analysis.  Note all the tweets are on one line.  This was required depending
+# your pycharm settings in order to view all tweets within the list.
+print(f'The results of the tweet_analysis using pipeline from  transformers is {results}', end = "\n\n")
+
+# Displays total percentage of violent and non-violent crimes using transformers.
 violent_pct_a, non_violent_pct_a = compute_percentages(results)
-print(f'Violent crime tweets:   {violent_pct_a:.2f}%')
-print(f'Non-violent crime tweets: {non_violent_pct_a:.2f}%')
-print()
-# "Non_Violent_Crimes_per_10_000_2010", "Violent_Crimes_per_10_000_2010", "Total_Crimes_per_10_000_2010",
-# "Unemployment_Aug_2010", "Non_Violent_Crimes_per_10_000_2022", "Violent_Crimes_per_10_000_2022",
-# "Total_Crimes_per_10_000_2022"
-# Step 2 compare the results of the tweet analysis compared to the reported violent and non-violent crime rate
-# based on mass.gov that persists in the mysql db.
-# recorded_violent_crimes_2022 = data[['non-violent', 'Non_Violent_Crimes_per_10_000_2022']]
+print(f'Violent crime tweets using hugging face transformers:   {violent_pct_a:.2f}%')
+print(f'Non-violent crime tweets using hugging face transformers: {non_violent_pct_a:.2f}%', end= "\n\n")
 
-# header = data.head() # Get a concise summary of the DataFrame
-# info = data.info() # Generate descriptive statistics
-# statistics = data.describe(include='all') # Check for missing values in each column
-# missing_value = data.isnull().sum() # Calculate the percentage of missing values per column
-# missing_percentage = data.isnull().mean() * 100
-# analysis_dict = {'header': header, 'info': info, 'statistics': statistics, 'missing_value': missing_value,
-#                  'missing_percentage': missing_percentage}
-#
-# for key in analysis_dict:
-#     print(f'For {key} the result is {analysis_dict[key]}')
+# Step 4 - Call MA crime data for the city of Boston from the database and assign the data to it's
+# Field within a dictionary.
+query = """
+SELECT population, population_2022, Non_Violent_Crimes_per_10_000_2010, Violent_Crimes_per_10_000_2010, Total_Crimes_per_10_000_2010,
+Unemployment_Aug_2010, Non_Violent_Crimes_per_10_000_2022, Violent_Crimes_per_10_000_2022, Total_Crimes_per_10_000_2022, Unemployment_Rate_2022 
+FROM ma_crime_unemployment
+Where city = 'Boston'
+"""
 
-# Step 2: Classify tweets
-# tweet_analysis = classify_tweets(data)
+print(f'Fetching the data from the ma_crime_unemployment table in the database')
+data = db_connector.fetch_data(query)
+
+columns = [
+    "Non_Violent_Crimes_per_10_000_2010", "Violent_Crimes_per_10_000_2010", "Total_Crimes_per_10_000_2010",
+    "Unemployment_Aug_2010", "Non_Violent_Crimes_per_10_000_2022", "Violent_Crimes_per_10_000_2022",
+    "Total_Crimes_per_10_000_2022", "Unemployment_Rate_2022"
+]
+
+df = pd.DataFrame(data, columns=columns)
+print("Data fetched successfully:")
+# print(df)
+
+# Initialize dictionary for access to the query values outside of the for loop
+crime_data_dict = {}
+
+Non_Violent_Crimes_per_10_000_2010 = 0
+Violent_Crimes_per_10_000_2010 = 0
+Total_Crimes_per_10_000_2010 = 0
+Unemployment_Aug_2010 = 0.0
+Non_Violent_Crimes_per_10_000_2022 = 0
+Violent_Crimes_per_10_000_2022 = 0
+Total_Crimes_per_10_000_2022 = 0
+Unemployment_Rate_2022 = 0.0
+population = 0
+population_2022 = 0
+
+# iterate through this data and print it in the console to compare the data.
+# Notice that unemployment rate between these two times is significantly different.
+# Also, notice that the total number of crime directly correlates with unemployment rates.
+for index, row in df.iterrows():
+    crime_data_dict[index] = {column: row[column] for column in df.columns}
+    # print(f"Row {index} details:")
+
+    for column in df.columns:
+        if column == "Non_Violent_Crimes_per_10_000_2010":
+            Non_Violent_Crimes_per_10_000_2010 = row[column]
+        elif column == "Violent_Crimes_per_10_000_2010":
+            Violent_Crimes_per_10_000_2010 = row[column]
+        elif column == "Total_Crimes_per_10_000_2010":
+            Total_Crimes_per_10_000_2010 = row[column]
+        elif column == "Unemployment_Aug_2010":
+            Unemployment_Aug_2010 = row[column]
+        elif column == "Non_Violent_Crimes_per_10_000_2022":
+            Non_Violent_Crimes_per_10_000_2022 = row[column]
+        elif column == "Violent_Crimes_per_10_000_2022":
+            Violent_Crimes_per_10_000_2022 = row[column]
+        elif column == "Total_Crimes_per_10_000_2022":
+            Total_Crimes_per_10_000_2022 = row[column]
+        elif column == "Unemployment_Rate_2022":
+            Unemployment_Rate_2022 = row[column]
+        elif column == "population":
+            population = row[column]
+        elif column == "Population_2022":
+            population_2022 = row[column]
+
+        # print(f" {column}: {row[column]}")
+
+violent_crime_pct_2010 = round(Non_Violent_Crimes_per_10_000_2010/Total_Crimes_per_10_000_2010 * 10, 1)
+non_violent_crimes_pct_2010 = round(Violent_Crimes_per_10_000_2010/Total_Crimes_per_10_000_2010 * 10, 1)
+
+violent_crime_pct_2022 = round(Violent_Crimes_per_10_000_2022/Total_Crimes_per_10_000_2022 * 10, 1)
+non_violent_crimes_pct_2022 = round(Non_Violent_Crimes_per_10_000_2022/Total_Crimes_per_10_000_2022 * 10, 1)
+
+# Print 2010 Boston crime and unemployment data
+print(f'''
+Crime information for city of Boston in 2010: 
+Population: {population}
+Unemployment rate: {round(Unemployment_Aug_2010,1)}
+Non-violent crimes per 10,000: {Non_Violent_Crimes_per_10_000_2010}
+Violent crimes per 10,000: {Violent_Crimes_per_10_000_2010}
+Total Crimes per 10,000: {int(Total_Crimes_per_10_000_2010)}  
+Percentage of violent crime based on total crime in 2010: {violent_crime_pct_2010}
+Percentage of non-violent crime based on total crime in 2010: {non_violent_crimes_pct_2010}
+''')
+
+# Print 2022 Boston crime and unemployment data
+print(f'''
+Crime information for city of Boston in 2022: 
+Population: {population_2022}
+Unemployment rate: {round(Unemployment_Rate_2022,1)}
+Total non-violent crimes per 10,000: {Non_Violent_Crimes_per_10_000_2022}
+Total violent crimes per 10,000: {Violent_Crimes_per_10_000_2022}
+Total crimes per 10,000: {int(Total_Crimes_per_10_000_2022)}.  
+Percentage of violent crime based on total crime in 2022: {int(violent_crime_pct_2022)}
+Percentage of non-violent crime based on total crime in 2022: {int(non_violent_crimes_pct_2022)}
+''')
+
+# Print the difference between the data from both years
+print('Difference between crime and unemployment data in Boston based on 2010 vs 2022:')
+print(f'''
+'Difference between crime and unemployment data in Boston based on 2010 vs 2022:
+unemployment rate in 2022 vs 2010: {round(Unemployment_Aug_2010 - Unemployment_Rate_2022, 1)} less.
+Total crimes per 10,000 in 2022 vs 2010: {int(Total_Crimes_per_10_000_2010 - Total_Crimes_per_10_000_2022)} less.  
+Percentage of violent crime in 2022 vs 2010: {int(violent_crime_pct_2010 - Violent_Crimes_per_10_000_2022)} less.
+Non-violent crime in 2022 vs 2010: {int(Non_Violent_Crimes_per_10_000_2010 - Non_Violent_Crimes_per_10_000_2022)} less.
+''', end='\n\n')
 
 
+print('''Tweet values using the hugging face library.  
+Notice the difference between the 2022 percentages used by calculating the data using the information available
+on mass.gov website.  Then go back to the line in the terminal and take a look at the list of tweet dictionaries.  A
+You'll notice that according to Transformers many of the tweets were very close to going one way or another
+based on the analysis.''')
 
+print(f'''
+Violent crime tweets using hugging face transformers:   {violent_pct_a:.2f}%
+Non-violent crime tweets using hugging face transformers: {non_violent_pct_a:.2f}%
+vs.
+Percentage of violent crime based on total crime in 2022: {int(violent_crime_pct_2022)}
+Percentage of non-violent crime based on total crime in 2022: {int(non_violent_crimes_pct_2022)}
+''')
 
-# classifier = CrimeClassifier()
-# classified_data = classifier.classify_dataset(data, text_column='crime_tweets') # Classify as either violent or non-violent crime
-#
-# # Step 3: Display results
-# print(classified_data['crime_type'].value_counts())
 
 
 
